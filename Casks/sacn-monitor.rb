fcbfc083125928b6e5f8683c573f47128c17ef4e
Alban Moreon <alban.moreon@protonmail.com>
2025-01-18 23:13:30 -0500

    Release sACN-Monitor v1.4.0

--- Casks/sacn-monitor.rb
+++ Casks/sacn-monitor.rb
@@ -1,6 +1,6 @@
 cask "sacn-monitor" do
-  version "1.3.0"
-  sha256 "b3e5b68b6b595dfc8c75ff826d8a5899a8c6d23488719a1aecf085225b1b8459"
+  version "1.4.0"
+  sha256 "b84d616b8a4108415a137be0fda8e8ff5230eac45950a7ce3bcd09ae0c4c3f98"
   url "https://gitlab.com/patopest/sacn-monitor/-/releases/v#{version}/downloads/macos-universal"
   
   name "sACN Monitor"
