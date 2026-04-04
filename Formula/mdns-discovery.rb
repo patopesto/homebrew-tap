6028b2ebebc15c862ad1806358a3443d5a4b34e3
Alban Moreon <alban.moreon@protonmail.com>
2026-04-03 23:13:27 -0400

    Release mdns-discovery v1.1.0 with formula to build from source

--- Formula/mdns-discovery.rb
+++ Formula/mdns-discovery.rb
@@ -1,36 +1,25 @@
 class MdnsDiscovery < Formula
-  desc "A small TUI app to discover mDNS/Zeroconf/Bonjour services and devices on your network.  "
+  desc "TUI for discovering mDNS/Zeroconf/Bonjour devices on your local network"
   homepage "https://gitlab.com/patopest/mdns-discovery"
+  version "1.1.0"
+  url "https://gitlab.com/patopest/mdns-discovery/-/archive/v#{version}/mdns-discovery-v#{version}.tar.gz"
+  sha256 "48aec809d445db87df5405dd4af9457acad71177de1f57bdbe635738c56c5211"
   license "MIT"
-  version "v1.0.0"
+  head "https://gitlab.com/patopest/mdns-discovery.git", branch: "master"
 
-  on_macos do
-    on_arm do
-      url "https://gitlab.com/patopest/mdns-discovery/-/releases/v1.0.0/downloads/mdns-discovery_1.0.0_Darwin_arm64.tar.gz"
-      sha256 "b4286ffccf74908ce1100c037d122fe2b945e68b1cf7134b5824fe74765ec1cb"
-    end
-    on_intel do
-      url "https://gitlab.com/patopest/mdns-discovery/-/releases/v1.0.0/downloads/mdns-discovery_1.0.0_Darwin_x86_64.tar.gz"
-      sha256 "418f2acc729d96e39ebb68e22c9801f275cfe5d9e9383a45f7d055f4312d370f"
-    end
-  end
-
-  on_linux do
-    on_intel do
-      url "https://gitlab.com/patopest/mdns-discovery/-/releases/v1.0.0/downloads/mdns-discovery_1.0.0_Linux_x86_64.tar.gz"
-      sha256 "04a66b54bb24a76479191fe4a0c3b99f4dce743d33392fa62127ec3158f2461b"
-    end
-    on_arm do
-      if Hardware::CPU.is_64_bit?
-        url "https://gitlab.com/patopest/mdns-discovery/-/releases/v1.0.0/downloads/mdns-discovery_1.0.0_Linux_arm64.tar.gz"
-        sha256 "29b953a9721b81ca507bd32c1afa209f5e915b134c6c76c17f0321e4ddb7cd35"
-      end
-    end
-  end
-
+  depends_on "go" => :build
 
   def install
-    bin.install "mdns-discovery"
+    ldflags = %W[
+      -s -w
+      -X main.Version=#{version}
+      -X main.Branch=master
+      -X main.BuildDate=#{time.iso8601}
+    ]
+    system "go", "build", "-trimpath", *std_go_args(ldflags:), "."
   end
 
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mdns-discovery --version")
+  end
 end
